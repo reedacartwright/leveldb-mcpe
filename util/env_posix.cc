68407886acb08e917f326d42261d8eb6a4091eb3
Reed A. Cartwright <cartwright@asu.edu>
2025-03-24 17:27:51 -0700

    Skip usages of stderr and abort in production code

--- util/env_posix.cc
+++ util/env_posix.cc
@@ -519,10 +519,12 @@ class PosixEnv : public Env {
  public:
   PosixEnv();
   ~PosixEnv() override {
+#if !defined(NDEBUG)
     static const char msg[] =
         "PosixEnv singleton destroyed. Unsupported behavior!\n";
     std::fwrite(msg, 1, sizeof(msg), stderr);
     std::abort();
+#endif // !defined(NDEBUG)
   }
 
   Status NewSequentialFile(const std::string& filename,
